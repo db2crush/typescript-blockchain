e37b89a65b9dca70c7aafea1cc9f656dbcb269e7
delbert <delbertvibes@gmail.com>
2019-01-20 22:59:13 +0900

    start of blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,2 @@
-interface Human  {
-    name: string,
-    age: number,
-    gender: string
-}
-
-const person = {    
-    name: "delbert",
-    age: 28,
-    gender: "male"
-} 
-
-const Hi = (person: Human): string => {
-    return `Hello ${person.name}, your age is ${person.age} and your gender is a ${person.gender} !`;
-}
-
-Hi(person);
 
 export {};
